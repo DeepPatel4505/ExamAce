e71a173a3ed7d80824178a3c80c0a23c4ecbf47e
Deep Patel <deeppatel5501@gmail.com>
2024-08-20 22:30:53 +0530

    FileStructure_withMain

---
+++ server/includes/header.php
@@ -0,0 +1,19 @@
+<header>
+<nav class="navbar">
+    <div class="navbar-logo">
+        Exam<span>Ace</span>
+    </div>
+    <div class="navbar-list">
+        <ul>
+            <li><a href="/ExamAce">Home</a></li>
+            <li><a href="/">About Us</a></li>
+            <li><a href="/">Contact Us</a></li>
+            <li><a href="/"></a></li>
+        </ul>
+    </div>
+    <div class="navbar-searchbar">
+        <input type="text">
+        <span>search</span>
+    </div>
+</nav>
+</header>
